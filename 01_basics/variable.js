313f0ad83fef9b6fd963cdc412c932fdb53d88cf
mlengineer666 <149672581+mlengineer666@users.noreply.github.com>
2025-04-21 18:18:28 +0000

    another changes

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,3 +1,16 @@
-const accountId = 711227
+const accountId = 711227  // it cann't be changed
 let accountEmail = "Prabirkayal@gmail.com"
-var account
+var accountPass = 28849  // we don't use this var , it has scope issue
+accountType = "Savings" 
+
+//method 1 to console data
+console.log(accountId)
+
+//method 2 to console data
+console.table([accountId,accountEmail,accountPass,accountType])
+
+/*
+// e.g-> of const this cann't be changed
+accountId = 888888 // it will didn't throw an error
+console.log(accountId) // but when it will cosole it will throw an error
+*/
