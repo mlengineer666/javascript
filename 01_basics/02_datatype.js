c92018e5107ee73931bb344b9f09bfc9c60f729e
mlengineer666 <149672581+mlengineer666@users.noreply.github.com>
2025-04-21 18:53:11 +0000

    changes_successfull

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -3,3 +3,24 @@
 // alert("hello")  //we ar using node.js ,not browser
 console.log(2+4)
 console.log("Prabir")
+
+
+let name = "Prabir" //string datatype
+let age = 20 // number type
+let isLoggedIn = false //boolean type
+
+/* 
+    1. number => 2 to power 53
+    2. bigint
+    3. sting =>"" or ''
+    4. boolean => true/false
+    5. null => standalone value
+    6. undefined =>
+
+*/
+
+// object
+console.log(typeof(name))
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof undefined)  // syntax 2 of typeof
