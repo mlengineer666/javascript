a7fe390e99a5aea73b65d32f405533937b41e0d2
mlengineer666 <149672581+mlengineer666@users.noreply.github.com>
2025-04-21 19:18:47 +0000

    conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "34"
+// let score = 34
+// let score = "34abc"
+// let score = null
+// let score = undefined
+let score = "Prabir"
+
+// let isLoggedIn = 1 -> true
+// let isLoggedIn = "" -> false
+let isLoggedIn = "prabir" // -> true
+
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber) //-> 33
+console.log(typeof stringNumber) //-> string
