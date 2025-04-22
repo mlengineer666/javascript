02a099ce8c76f5ae9f65858fbd3464944839654b
S-A-N-D-I-P-A-N <149672581+mlengineer666@users.noreply.github.com>
2025-04-22 18:57:27 +0000

    add new changes

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -11,15 +11,44 @@ let isLoggedIn = "prabir" // -> true
 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-console.log(typeof score)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(stringNumber) //-> 33
-console.log(typeof stringNumber) //-> string
+// console.log(stringNumber) //-> 33
+// console.log(typeof stringNumber) //-> string
+
+
+// ********************* OPERATIONS **************************
+let value = 3
+let negValue = -value
+
+// console.log(negValue)
+
+// console.log('mydir'/3) // NaN
+let op1 = '1'+'2' //12
+let op2 = '1'+2+3
+let op3 = 1+3+4+'4' //first data are integers so it will add that value
+let op4 = '4'+3+4 // it will concat because of string
+
+// console.log(typeof op2)
+// console.log(op4)
+// console.log(2+4*5%3)  // higher precendence * and % equal then +,- so,4*5 =20 then 20%3 = 2 then 2+2 = 4
+
+// console.log(+true)
+// console.log(+'')
+
+
+let num1,num2,num3
+num1=num2=num3 = 2+2
+// console.table([num1,num2,num3])
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
