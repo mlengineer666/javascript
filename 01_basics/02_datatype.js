bf75beac6d1f888f7d1e2771ad758010e8ab36aa
S-A-N-D-I-P-A-N <149672581+mlengineer666@users.noreply.github.com>
2025-04-22 19:51:34 +0000

    datatype_summary

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -20,7 +20,41 @@ let isLoggedIn = false //boolean type
 */
 
 // object
-console.log(typeof(name))
-console.log(typeof(age))
-console.log(typeof(null))
-console.log(typeof undefined)  // syntax 2 of typeof
+// console.log(typeof(name))
+// console.log(typeof(age))
+// console.log(typeof(null))
+// console.log(typeof undefined)  // syntax 2 of typeof
+
+
+// Primitive dataType
+// 7 types => Boolean,Undefined,Null,Number,String,Symbol,BigInt
+
+//Reference type or Non-Primitive => Array,BObjects,Functions
+
+const score = 100
+const sc = 100.4
+
+// symbol
+const Id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(Id == anotherId)
+
+const bigNumber = 488882348486272734848449493n
+console.log(typeof bigNumber)
+
+//Array
+const heroes = ['shaktiman','nagaraj','doga'];
+//Objects
+let myObj = {
+    name:"Prabir",
+    age:27
+}
+
+//basic function
+const myFunc = function(){
+    console.log("Prabir")
+}
+console.log(myFunc())
+
+console.log(typeof myFunc)
