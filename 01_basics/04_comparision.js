545f130394361ff06fcac204d60073cd9791e4f1
S-A-N-D-I-P-A-N <149672581+mlengineer666@users.noreply.github.com>
2025-04-22 19:17:28 +0000

    add new file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2>=1)
+
+// console.log("p">1)
+// console.log('2'>1)
+// console.log('1'>1)
+
+
+// console.log(null > 0) // false because null treat as 0
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(0 != 0)
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+// console.log(undefined != 0)
+
+console.log("2"==2) //true for the conversion
+console.log('2'===2) // here === sign check the datatype is same or not
+
